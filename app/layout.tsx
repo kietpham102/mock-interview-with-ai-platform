0964a3fc725c99e2e287571a80fcbc9b5d844ccc
Kiet Pham <anhkietpham.hcmus@gmail.com>
2025-03-31 11:15:10 +0700

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,17 @@ const monaSans = Mona_Sans({
 export const metadata: Metadata = {
   title: "Mock Interview Platform",
   description: "An AI-powered platform for preparing mock interviews",
+  openGraph: {
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/og-ai.png`,
+        alt: 'AI talking',
+        width: 1200,
+        height: 1200,
+      },
+    ],
+  },
+  
 };
 
 export default function RootLayout({
